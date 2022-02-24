d69f5f209dfd96afd2751917e7f169455d25424e
Zakiirim <Raffaele.DiConstanzo@planixs.com>
2022-02-24 10:28:50 +0100

    Created Pricing functions

---
+++ src/runSimpleMonteCarlo.h
@@ -0,0 +1,12 @@
+#ifndef RUNSIMPLEMONTECARLO_H
+#define RUNSIMPLEMONTECARLO_H
+
+// prototype of the function
+double runSimpleMonteCarlo(double Expiry,
+						    double Strike,
+						    double Spot,
+						    double Vol,
+						    double r,
+						    unsigned long int NumberOfPaths);
+
+#endif
